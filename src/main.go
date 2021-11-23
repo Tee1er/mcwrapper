83066ae72f60d48208290dc1aea74bf06d10ee12
Tyler Song <tyler.x.song@gmail.com>
2021-11-22 20:28:00 -0500

    Basic starting of server as child process.

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/fatih/color"
@@ -31,7 +32,7 @@ func contains(slice []string, element string) bool {
 		}
 	}
 	return false
-} 
+}
 
 func getInput(prompt string) string {
 	reader := bufio.NewReader(os.Stdin)
@@ -48,48 +49,11 @@ func getUrl(prompt string) string {
 	return result
 }
 
-func main() {
-	c := color.New(color.FgCyan, color.Bold)
-	c.Println("-- MCWrapper v0.1-alpha CLI -- \n")
-	for {
-		input := getInput("> ")
-
-		switch input {
-			case "init":
-				url := getUrl("Enter URL for server download: ")
-				data := getServer(url)
-				getServer(url)
-				unzip(data, "../server")
-			case "update":
-				url := getUrl("Enter URL for server download: ")
-				data := getServer(url)
-				fmt.Println("Moving data to temporary dir.")
-				moveServer("../server", "../temp")
-				err := os.RemoveAll("../server")
-				check(err)
-				fmt.Println("Extracting new server files.")
-				unzip(data, "../server")
-				fmt.Println("Moving data back.")
-				moveServer("../temp", "../server")
-
-
-			case "help":
-				fmt.Println("\tCOMMANDS:\n")
-				fmt.Println("\tinit\tDownloads and extracts the latest server version.")
-				fmt.Println("\tupdate\tDownloads, extracts, and updates the server to the latest version.")
-				fmt.Println("\texit\tExits the program.")
-			case "exit":
-				os.Exit(0)
-			default:
-				fmt.Println("\tInvalid command. Type 'help' for a list of commands.")
-			}
-	}
-}
-
 func getServer(url string) []byte {
 	//"https://minecraft.azureedge.net/bin-win/bedrock-server-1.17.41.01.zip"
 	// Latest ZIP file for server release ... unsure how to get the latest version since version numbers are not always in order. Could ask user for version #.
-	resp, err := http.Get(url) 
+	// Alternatively, could use GH to get the latest version
+	resp, err := http.Get(url)
 
 	check(err)
 
@@ -126,16 +90,82 @@ func moveServer(serverPath string, destPath string) {
 	files, err := ioutil.ReadDir(serverPath)
 	check(err)
 
-	for _, file := range files {
-		if !file.IsDir() && contains(copyFiles, file.Name())  {
-			err := os.Rename(serverPath + "/" + file.Name(), destPath + "/" + file.Name())
-			check(err)
-			fmt.Printf("Copied file %s to %s\n", file.Name(), destPath + "/" + file.Name() )
-		} else if file.IsDir() && contains(copyDirs, file.Name()) {
-			err := copy.Copy(serverPath + "/" + file.Name(), destPath + "/" + file.Name())
-			check(err)
-			fmt.Printf("Copied directory %s to %s\n", file.Name(), destPath + "/" + file.Name())
+	// Copy all files from source to destination. TODO: clean this up.
 
+	for _, file := range files {
+		if !file.IsDir() && contains(copyFiles, file.Name()) {
+			err := os.Rename(serverPath+"/"+file.Name(), destPath+"/"+file.Name())
+			check(err)
+			fmt.Printf("Copied file %s to %s\n", file.Name(), destPath+"/"+file.Name())
+		} else if file.IsDir() && contains(copyDirs, file.Name()) {
+			err := copy.Copy(serverPath+"/"+file.Name(), destPath+"/"+file.Name())
+			check(err)
+			fmt.Printf("Copied directory %s to %s\n", file.Name(), destPath+"/"+file.Name())
+
+		}
+	}
+}
+
+func startServer( /**exec.Cmd, *io.ReadCloser, *io.WriteCloser*/ ) {
+	color.Yellow("Starting server.")
+	mcServer := exec.Command("../server/bedrock_server.exe")
+
+	stdout, _ := mcServer.StdoutPipe()
+	// stdin, _ := mcServer.StdinPipe()
+
+	mcServer.Start()
+	color.Blue("Server started.")
+	// in, _ := mcServer.StdinPipe()
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		m := scanner.Text()
+		fmt.Println(m)
+	}
+}
+
+func main() {
+	c := color.New(color.FgCyan, color.Bold)
+	c.Println("-- MCWrapper v0.1-alpha CLI -- \n")
+	for {
+		input := getInput("> ")
+
+		switch input {
+		case "init":
+			url := getUrl("Enter URL for server download: ")
+			data := getServer(url)
+			getServer(url)
+			unzip(data, "../server")
+		case "update":
+			url := getUrl("Enter URL for server download: ")
+			data := getServer(url)
+
+			fmt.Println("Moving data to temporary dir.")
+
+			moveServer("../server", "../temp")
+			os.RemoveAll("../server")
+
+			fmt.Println("Extracting new server files.")
+
+			unzip(data, "../server")
+
+			fmt.Println("Moving data back.")
+
+			moveServer("../temp", "../server")
+			os.RemoveAll("../temp")
+
+		case "help":
+			fmt.Println("\tCOMMANDS:\n")
+
+			fmt.Println("\tinit\tDownloads and extracts the latest server version.")
+			fmt.Println("\tupdate\tDownloads, extracts, and updates the server to the latest version. Preserves worlds and some other config files.")
+			fmt.Println("\run\tRuns the server.")
+			fmt.Println("\texit\tExits the program.")
+		case "run":
+			startServer()
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("\tInvalid command. Type 'help' for a list of commands.")
 		}
 	}
 }
